542c9d40a44540cd552d6bb1f28a31eb43c504a0
Ocloo Priscilla <ocloopriscy98@gmail.com>
2021-03-27 06:05:35 +0000

    react project initialized

--- src/App.js
+++ src/App.js
@@ -23,22 +23,66 @@
 //   }
 // }
 
-function App(){
-  const [state,setState] = useState({age: 20})
+//  function App(){
+//    const [state,setState] = useState({age: 20})
 
-  const handleClick = () => {
-    let newAge = state.age * 5;
-    setState({ age: newAge});
-    console.log('button clicked');
-  };
+//    const handleClick = () => {
+//      let newAge = state.age * 5;
+//      setState({ age: newAge});
+//      console.log('button clicked');
+//    };
 
-  return (
-    <div>
-    <h1>Age is {state.age}</h1>
-    <button onClick={handleClick}>Click me!!!</button>
-  </div>
-  )
+//   return (
+//      <div>
+//      <h1>Age is {state.age}</h1>
+//      <button onClick={handleClick}>Click me!!!</button>
+//   </div>
+//    )
   
-}
+//  }
 
- export default App;
+//   export default App;
+
+// class App extends Component{
+//   constructor(){
+//     super();
+//     this.state = {age:50}
+//   }
+//   handleClick = () => {
+//     console.log('button clicked');
+//     let newAge = this.state.age + 10;
+//     this.setState( {age:newAge})
+
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <h1>Age is {this.state.age}</h1>
+//         <button onClick={this.handleClick}>Click me!!!</button>
+//         </div>
+//     )
+//   }
+// }
+// export default App;
+
+function App(){
+      const [state,setState] = useState({age: 50})
+      const handleClick = () => {
+        let newAge = state.age + 10;
+        setState({ age: newAge});
+        console.log('button clicked');
+      };
+  
+     return (
+        <div>
+        <h1>Age is {state.age}</h1>
+        <button onClick={handleClick}>Click me!!!</button>
+     </div>
+      )
+    
+    }
+  
+     export default App;
+
+
